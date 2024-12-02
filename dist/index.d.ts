acb86b2cc3eadee671691fa99c116a7321469b5d
Haylee Schäfer <haylee@inventivetalent.org>
2024-12-02 16:49:52 +0100

    only export ts

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./RequestManager";
-export * from "./RequestConfig";
--- dist/util.d.ts
+++
@@ -1,4 +0,0 @@
-import { NetworkInterfaceInfo } from "node:os";
-export declare function isPublicNetworkInterface(address: NetworkInterfaceInfo): boolean;
-export declare function isPublicIPv4(address: string): boolean;
-export declare function isPublicIPv6(address: string): boolean;
